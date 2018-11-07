19b8ed4cd17b678522964b897ddc2fdb6897475f
Bernardo Raposo <braposo@gmail.com>
2018-11-07 22:47:35 +0000

    Improves examples page
    Adds more realistic examples

--- examples/App.js
+++ examples/App.js
@@ -1,64 +1,70 @@
 import React from "react";
-import { FullsizePicture } from "../src";
+import { Picture, FullsizePicture } from "../src";
 import cxs from "cxs/component";
+import { default as cxsStyles } from "cxs";
 
-const StyledPicture = cxs(FullsizePicture)({
-    opacity: 0.8,
+const styles = cxsStyles({
+    opacity: 0.5,
 });
 
 const Heading1 = cxs("h1")({
     position: "relative",
     textAlign: "center",
     color: "white",
+    marginTop: 50,
 });
 
 const App = () => (
     <div>
-        <div style={{ height: 200 }}>
-            <StyledPicture
+        <h2>Picture with art direction</h2>
+        <div>
+            <Picture
                 sources={[
                     {
                         srcSet:
-                            "https://edited.com/static/img/homepage/edited-hero-table-wide.jpg",
-                        media: "(max-width: 420px)",
+                            "https://placeholdit.co//i/400x400?text=Mobile%20image&bg=450000",
+                        media: "(max-width: 380px)",
                     },
                     {
                         srcSet:
-                            "https://edited.com/static/img/homepage/edited-hero-table-wide.jpg",
+                            "https://placeholdit.co//i/500x250?text=Normal%20image&bg=450000 1x, https://placeholdit.co//i/1000x500?text=Retina%20image&bg=450000 2x",
+                    },
+                ]}
+            />
+        </div>
+        <h2>Full-height Picture with art direction</h2>
+        <div style={{ height: 400 }}>
+            <FullsizePicture
+                sources={[
+                    {
+                        srcSet:
+                            "https://placeholdit.co//i/400x400?text=Mobile%20image",
+                        media: "(max-width: 380px)",
+                    },
+                    {
+                        srcSet:
+                            "https://placeholdit.co//i/500x250?text=Normal%20image 1x, https://placeholdit.co//i/1000x500?text=Retina%20image 2x",
                     },
                 ]}
                 cover="height"
                 center={true}
             />
         </div>
-        <div style={{ height: 200 }}>
+        <h2>Full-width Picture with content</h2>
+        <div style={{ height: 400 }}>
             <FullsizePicture
-                sources={[
-                    {
-                        srcSet:
-                            "https://edited.com/static/img/homepage/edited-hero-table-wide.jpg",
-                        media: "(max-width: 420px)",
-                    },
-                    {
-                        srcSet:
-                            "https://edited.com/static/img/homepage/edited-hero-table-wide.jpg",
-                    },
-                ]}
-                cover="height"
+                src="https://images.unsplash.com/photo-1470619549108-b85c56fe5be8?dpr=2&auto=format&w=1024&h=1024"
                 center={true}
-            />
-        </div>
-        <div style={{ height: 200 }}>
-            <FullsizePicture
-                sources={[
-                    {
-                        srcSet:
-                            "https://images.unsplash.com/photo-1470619549108-b85c56fe5be8?dpr=2&auto=format&w=1024&h=1024",
-                    },
-                ]}
             >
                 <Heading1 className="App-title">This is a heading</Heading1>
             </FullsizePicture>
+        </div>
+        <h2>Styled Picture</h2>
+        <div>
+            <Picture
+                className={styles}
+                src="https://placeholdit.co//i/500x250?text=Normal%20image&bg=004500 1x, https://placeholdit.co//i/1000x500?text=Retina%20image&bg=004500 2x"
+            />
         </div>
     </div>
 );
